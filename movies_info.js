99c49572680347cbdf4d22b1b3467c176973dcfb
KKazane <k.kazaneko@gmail.com>
2022-06-18 19:50:19 +0900

    Make another file for informations

---
+++ movies_info.js
@@ -0,0 +1,258 @@
+const movies = [
+  {
+    name: "3idiots",
+    time: 171,
+    genres: "Comedy",
+    release: 2009,
+    countries: "India",
+    director: "Rajkumar Hirani"
+  },
+  {
+    name: "The Darjeeling Limited",
+    time: 91,
+    genres: "Comedy",
+    release: 2007,
+    countries: "United States",
+    director: "Wes Anderson"
+  },
+  {
+    name: "Bajrangi Bhaijaan",
+    time: 160,
+    genres: "Drama",
+    release: 2015,
+    countries: "India",
+    director: "Kabir Khan"
+  },
+  {
+    name: "Promising Young Woman",
+    time: 113,
+    genres: ["Drama", "Thriller"],
+    release: 2020,
+    countries: ["United Kingdom", "United States"],
+    director: "Emerald Fennel"
+  },
+  {
+    name: "Gunpowder Milkshake",
+    time: 114,
+    genres: ["Action", "Adventure", "Thriller"],
+    release: 2021,
+    countries: ["Germany", "France", "United States"],
+    director: "Navot Papushado"
+  },
+  {
+    name: "Ocean's 8",
+    time: 120,
+    genres: ["Action", "Comedy", "Thriller"],
+    release: 2018,
+    countries: "United States",
+    director: "Gary Ross"
+  },
+  {
+    name: "Little Women",
+    time: 135,
+    genres: ["Drama", "Romance"],
+    release: 2019,
+    countries: "United States",
+    director: "Greta Gerwig"
+  },
+  {
+    name: "20th Century Women",
+    time: 118,
+    genres: ["Comedy", "Drama"],
+    release: 2016,
+    countries: "United States",
+    director: "Mike Mills"
+  },
+  {
+    name: "Onneli ja Anneli",
+    time: 80,
+    genres: "Children's/Family",
+    release: 2014,
+    countries: "Finland",
+    director: "Saara Centell"
+  },
+  {
+    name: "God Help the Girl",
+    time: 111,
+    genres: ["Comedy", "Drama", "Musical"],
+    release: 2014,
+    countries: "United Kingdom",
+    director: "Stuart Murdoch"
+  },
+  {
+    name: "Short Term12",
+    time: 96,
+    genres: "Drama",
+    release: 2013,
+    countries: "United States",
+    director: "Destin Daniel Cretton"
+  },
+  {
+    name: "Fantastic Planet",
+    time: 72,
+    genres: "Science Fiction",
+    release: 1973,
+    countries: ["Czechia", "France"],
+    director: "René Laloux"
+  },
+  {
+    name: "Paterson",
+    time: 118,
+    genres: ["Comedy", "Drama"],
+    release: 2016,
+    countries: ["Germany", "France", "United States"],
+    director: "Jim Jarmusch"
+  },
+  {
+    name: "Bagdad Cafe",
+    time: 91,
+    genres: ["Comedy", "Drama"],
+    release: 1988,
+    countries: ["Germany", "United States"],
+    director: "Percy Adlon"
+  },
+  {
+    name: "Frances Ha",
+    time: 85,
+    genres: ["Comedy", "Drama"],
+    release: 2013,
+    countries: "United States",
+    director: "Noah Baumbach"
+  },
+  {
+    name: "Charlie's Angels",
+    time: 118,
+    genres: ["Action", "Comedy"],
+    release: 2019,
+    countries: "United States",
+    director: "Elizabeth Banks"
+  },
+  {
+    name: "Big Fish",
+    time: 125,
+    genres: ["Comedy", "Drama"],
+    release: 2003,
+    countries: "United States",
+    director: "Tim Burton"
+  },
+  {
+    name: "Mad Max: Fury Road",
+    time: 120,
+    genres: "Action",
+    release: 2015,
+    countries: ["American Samoa", "Australia", "United States", "South Africa"],
+    director: "George Miller"
+  },
+  {
+    name: "Les Misérables",
+    time: 150,
+    genres: "Musical",
+    release: 2012,
+    countries: ["United States", "United Kingdom"],
+    director: "Tom Hooper"
+  },
+  {
+    name: "Never Rarely Sometimes Always",
+    time: 101,
+    genres: "Drama",
+    release: 2020,
+    countries: ["United Kingdom", "United States"],
+    director: "Eliza Hittman"
+  },
+  {
+    name: "Thelma and Louise",
+    time: 130,
+    genres: ["Comedy", "Drama", "Action"],
+    release: 1991,
+    countries: ["Canada", "France", "United States"],
+    director: "Ridley Scott"
+  },
+  {
+    name: "Fried Green Tomatoes",
+    time: 130,
+    genres: ["Comedy", "Drama"],
+    release: 1991,
+    countries: ["United Kingdom", "United States"],
+    director: "Jon Avnet"
+  },
+  {
+    name: "Legally Bronde",
+    time: 96,
+    genres: "Comedy",
+    release: 2001,
+    countries: "United States",
+    director: "Robert Luketic"
+  },
+  {
+    name: "Babette's Feast",
+    time: 105,
+    genres: "Drama",
+    release: 1987,
+    countries: "Denmark",
+    director: "Gabriel Axel"
+  },
+  {
+    name: "Tully",
+    time: 95,
+    genres: ["Comedy", "Drama"],
+    release: 2018,
+    countries: "United States",
+    director: "Jason Reitman"
+  },
+  {
+    name: "I Feel Pretty",
+    time: 110,
+    genres: "Comedy",
+    release: 2017,
+    countries: "United States",
+    director: "Addy Kohn"
+  },
+  {
+    name: "Attila Marcel",
+    time: 106,
+    genres: "Drama",
+    release: 2013,
+    countries: "France",
+    director: "Sylvain Chomet"
+  },
+  {
+    name: "Hairspray",
+    time: 117,
+    genres: ["Comedy", "Drama"],
+    release: 2007,
+    countries: ["Canada", "United Kingdom", "United States"],
+    director: "Adam Shankman"
+  },
+  {
+    name: "The Handmaiden",
+    time: 145,
+    genres: ["Drama", "Romance"],
+    release: 2016,
+    countries: ["Korea", "South", "Norway"],
+    director: "Chan-wook Park"
+  },
+  {
+    name: "Tove",
+    time: 103,
+    genres: "Drama",
+    release: 2020,
+    countries: ["Finland", "United States"],
+    director: "Zaida Bergroth"
+  },
+  {
+    name: "Miss Marx",
+    time: 107,
+    genres: "Biography",
+    release: 2020,
+    countries: ["Italy", "Belgium"],
+    director: "Susanna Nicchiarelli"
+  },
+  {
+    name: "Kiss Me Before It Blows Up",
+    time: 105,
+    genres: ["Comedy", "Drama", "Romance"],
+    release: 2020,
+    countries: ["Germany", "Israel"],
+    director: "Shirel Peleg"
+  }
+  ];
